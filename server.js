08ffd0a7e6d2f7706413ea8ede21c3567de33d0c
hackpres <102390515+hackpres@users.noreply.github.com>
2022-07-17 12:22:48 -0400

    cloned repo, added starter code, began building server.js.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const data = require("./Develop/db/db.json");
+
+const app = express();
+const PORT = process.env.port || 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+    res.json(data)
+);
+
+app.post('/notes', (req, res) => {
+    console.info(`${req.method} request recieved to add a note`);
+    
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error occurred, note was unable to post.')
+    }
+});
+
+app.listen(PORT, () =>
+console.log(`App listening at ${PORT} 🚀`));
