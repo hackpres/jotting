620b8fd3d055dfc6c57277965ce7251cdfbcee74
hackpres <102390515+hackpres@users.noreply.github.com>
2022-07-20 14:12:47 -0400

    added some comments

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+    //commented out because its redundant. app.use(express.static('public')); already covers this get request.
+// app.get('/', (req, res) =>
+//     res.sendFile(path.join(__dirname + "/public/index.html"))
+// );
+
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname + "/public/notes.html"))
 );
