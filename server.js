13236a1bd9c74c9617521d74b9d29b68db8fb879
hackpres <102390515+hackpres@users.noreply.github.com>
2022-07-20 17:04:32 -0400

    working on heroku deployment

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
     //commented out because its redundant. app.use(express.static('public')); already covers this get request.
-// app.get('/', (req, res) =>
-//     res.sendFile(path.join(__dirname + "/public/index.html"))
-// );
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+);
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname + "/public/notes.html"))
